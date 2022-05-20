3f1d638edb216585ff8a497a9ed72b89eda7ed93
Yuval Ardenbaum <ardyuv001@myuct.ac.za>
2022-05-20 16:05:20 +0200

    Completed COIN.sol

--- contracts/COIN.sol
+++ contracts/COIN.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
 /// @title                      Pausable ERC20
 /// @notice                     Inherits OpenZeppelin ERC20 and pausable 
