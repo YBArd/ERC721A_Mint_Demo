9290e63a7e93e793d7c2ef94a3158cb2e6d06035
Yuval Ardenbaum <ardyuv001@myuct.ac.za>
2022-05-20 11:26:45 +0200

    Deleted greeter contract and added documentation for COIN and NFT

--- contracts/COIN.sol
+++ contracts/COIN.sol
@@ -5,19 +5,38 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title                      Pausable ERC20
+/// @notice                     Inherits OpenZeppelin ERC20 and pausable 
+///                             extension to enable/disable transfers
 contract COIN is ERC20, Pausable, Ownable {
+
+    /// CONSTRUCTOR ///
+
+    /// @notice                 Overridden constructor to initialize
+    ///                         ERC20 called COIN
+    /// @dev                    Premints 10000 tokens to msg.sender 
     constructor() ERC20("COIN", "COIN") {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        _mint(msg.sender, 10000);
     }
 
+    /// FUNCTIONS ///
+
+    /// @notice                 Freezes token contract to disable transfers
+    /// @dev                    Can only be called by contract owner
     function pause() public onlyOwner {
         _pause();
     }
 
+    /// @notice                 Unfreezes token contract to enable transfers
+    ///                         after being paused
+    /// @dev                    Can only be called by contract owner
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /// @notice                 Before transfer hook
+    /// @dev                    Checks to ensure token is not paused
+    ///                         before transfer
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,17 +6,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/// @title                      NFT
+/// @notice                     Inherits OpenZeppelin ERC721 and enables
+///                             minting with 1000 ERC20 tokens at coinAddress
 contract NFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    IERC20 public coinAddress;
-    uint256 public fee = 1000 * 10 ** 18;
 
-    Counters.Counter private _tokenIdCounter;
+    using Counters for Counters.Counter; // Safely increments tokenId during minting
+    
+    /// VARIABLES ///
 
+    IERC20 public coinAddress; // Address of ERC20 token to spend for minting
+
+    uint256 public fee = 1000; // Minting cost in tokens
+    
+    Counters.Counter private _tokenIdCounter; // NFT tokenId incrementer
+
+    /// CONSTRUCTOR ///
+
+    /// @notice                 Initializes ERC721 NFT called NFT with symbol
+    ///                         LNM (Linum)
+    /// @param _coinAddress     Contract address of ERC20 for minting
+    /// @dev                    Uses COIN to mint                   
     constructor(address _coinAddress) ERC721("NFT", "LNM") {
     	coinAddress = IERC20(_coinAddress);
     }
 
+    /// FUNCTIONS ///
+
+    /// @notice                 Minting function
+    /// @dev                    Overrides ERC721 safeMint() to accept 1000 COIN
     function safeMint() public {
     	coinAddress.transferFrom(msg.sender, address(this), fee);
         uint256 tokenId = _tokenIdCounter.current();
@@ -24,6 +42,8 @@ contract NFT is ERC721, Ownable {
         _safeMint(msg.sender, tokenId);
     }
 
+    /// @notice                 Token balance withdrawal 
+    /// @dev                    Allows contract owner to withdraw tokens from contract
     function rugTheFunds() public onlyOwner {
     	coinAddress.transfer(msg.sender, coinAddress.balanceOf(address(this)));
     }
